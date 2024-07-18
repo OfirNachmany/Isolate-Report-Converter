78b70d9f24c43c02b18d370f1f48d580b1659be0
OfirNachmany <144834127+OfirNachmany@users.noreply.github.com>
2024-07-18 17:18:00 +0300

    Update script.js
    fixing time doesnt convert correctly bug

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function convertExcelDate(excelDate) {
 
 // Function to format Date object to Israeli date format
 function formatDateToIsraeli(date) {
-    const day = String(date.getDate()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0') - 1;
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const year = String(date.getFullYear()).slice(-2); // Last two digits of the year
     return `${day}/${month}/${year}`;
@@ -35,7 +35,7 @@ function formatDateToIsraeli(date) {
 
 // Function to format time to Israeli format
 function formatTimeToIsraeli(date) {
-    const hours = String(date.getHours()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0') - 3;
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
